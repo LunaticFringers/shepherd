aa7d8d3e559d60117567f98dc4510dae21d3f624
Giuseppe Baccini <giuseppe.baccini@logobject.ch>
2025-03-21 18:01:15 +0100

    Initialize program's status on start
    - Ensure required resources (directories, files etc)
    - Load program's status by reading the configuration
    
    Fixes: #50
    Signed-off-by: Giuseppe Baccini <giuseppe.baccini@logobject.ch>

--- src/shpdctl.py
+++ src/shpdctl.py
@@ -20,20 +20,26 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import os
 from typing import Any
 
 import click
 
+from config import ConfigMng
 from database import DatabaseMng
 from environment import EnvironmentMng
 from service import ServiceMng
+from util import Util
 
 
 class ShepherdMng:
     def __init__(self):
-        self.databaseMng = DatabaseMng()
+        self.configMng = ConfigMng(os.path.join(("~"), ".shpd.conf"))
         self.environmentMng = EnvironmentMng()
         self.serviceMng = ServiceMng()
+        self.databaseMng = DatabaseMng()
+        self.cli_flags = {}
+        Util.ensure_dirs(self.configMng.constants)
 
     def create_cli(self):
         @click.group()
@@ -79,7 +85,7 @@ class ShepherdMng:
         def cli(**kwargs: dict[str, Any]):
             """Shepherd CLI:
             A tool to manage your environment, services, and database."""
-            pass
+            self.cli_flags = kwargs
 
         cli.add_command(self.create_db_commands(), "db")
         cli.add_command(self.create_env_commands(), "env")
